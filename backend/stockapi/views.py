f6e603a36daee943d621adacedba6922036812b0
Mehmet Ugur SATIR <musatir@gmail.com>
2023-05-05 11:20:06 +0300

    pruchase amount increases product stock

--- backend/stockapi/views.py
+++ backend/stockapi/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import DjangoModelPermissions
 from .models import Category, Firm, Brand, Product, Sale, Purchase
@@ -41,3 +43,19 @@ class PurchaseView(ModelViewSet):
     queryset = Purchase.objects.all()
     serializer_class = PurchaseSerializer
     permission_classes = [DjangoModelPermissions]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        purchase = request.data
+        print(purchase)
+        item = Product.objects.get(id=purchase["product_id"])
+        print(item)
+        item.stock += purchase["quantity"]
+        item.save()
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        serializer.save()
